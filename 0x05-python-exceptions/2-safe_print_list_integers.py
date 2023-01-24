fb2ff76c770e530e9d7725c3e99b41dd0ca7379a
kayc0des <k.boafo@alustudent.com>
2023-01-23 18:36:15 -0800

    exceptions

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,6 @@ def safe_print_list_integers(my_list=[], x=0):
             print('{:d}'.format(my_list[i]), end="")
             point_in += 1
         except (TypeError, ValueError):
-            continue
-
+            pass
     print()
     return point_in
